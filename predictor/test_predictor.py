430f65f23ba31485949fbaa2225fa233c4779c9a
Whu-wxy <641645560@qq.com>
2020-10-18 20:29:11 +0800

    add server_async

--- predictor/test_predictor.py
+++ predictor/test_predictor.py
@@ -15,37 +15,25 @@ class TestPredictor(Predictor):
 		pass
 
 	def _json_preprocessing(self, data):
-		# print("receive data: ", data)
-		# time.sleep(3)
+		print("receive data: ", data)
+		time.sleep(10)
+		return [1, 1, 0, 0]
 
-		img_save_path, result_save_path = get_img_save_dir('../')
-
-		try:
-			file_name = secure_filename(data['imgname'])
-			image = base64_to_cv2(data['image'])
-			if allowed_file(file_name):
-				file_name = "123_"+file_name
-				save_img(image, file_name, img_save_path)
-				print('file saved to %s' % img_save_path)
-			return [0, 0, 10, 0, 10, 10, 0, 10]
-		except:
-			return []
-
-
-		# upload_file = request.files['file']
-		# old_file_name = secure_filename(upload_file.filename)
-		# print(old_file_name)
-		# if upload_file:
-		# 	file_path = os.path.join('./', "123_"+old_file_name)
-		# 	upload_file.save(file_path)
-		# 	print('file saved to %s' % file_path)
-		# 	print('success!')
-		# 	return 'success'
-		# else:
-		# 	return 'failed'
+		# img_save_path, result_save_path = get_img_save_dir('../')
 		#
-		# return data
+		# try:
+		# 	file_name = secure_filename(data['imgname'])
+		# 	image = base64_to_cv2(data['image'])
+		# 	if allowed_file(file_name):
+		# 		file_name = "123_"+file_name
+		# 		save_img(image, file_name, img_save_path)
+		# 		print('file saved to %s' % img_save_path)
+		# 	return [0, 0, 10, 0, 10, 10, 0, 10]
+		# except:
+		# 	return []
+
 
 	def _predict_instance(self, instance):
+		print('after sleep: ', instance)
 		return {"result":[{"position":instance, "text":"识别结果1"}]}
 
