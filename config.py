4e55abf3709f6d56ad1abb78c45b94951413d78b
Whu-wxy <641645560@qq.com>
2020-11-07 20:01:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 PREDICTOR = 'ancient-chinese'    # detection  ancient-chinese  recognize recognize_batch  test
 PORT = 8009
 
-DETECTION_MODEL_PATH = '../data/mobilenet_our.onnx'  #./models/mobilenet.onnx
+DETECTION_MODEL_PATH = './data/mobilenet_our.onnx'  #./models/mobilenet.onnx
 MAX_LONG_SIZE = 2500    # None
 IMG_SCALE = 1.5
-POST_DB = False
+POST_DB = True
 D_MAX = 0.56
 
 #DB
@@ -17,9 +17,7 @@ FULL_TH = 0.8
 
 
 RECOGNITION_MODEL_NAME = 'inception_v4'  # resnet_v2_50
-RECOGNITION_MODEL_PATH = '../../data/recognize/train_logs_resnet_v2_502/'  #./models/
-
-
+RECOGNITION_MODEL_PATH = './data/recognize/inception/'
 RECOG_IMG_SHAPE = 235  #224
 
 
@@ -27,7 +25,7 @@ DB_PATH = './data/database.db'
 
 SAVE_ROOT_PATH = '../users_data'
 
-FONT_PATH = '../data/NotoSansCJK-Regular.ttc'
+FONT_PATH = './data/NotoSansCJK-Regular.ttc'
 
-SSL_KEY =  '' # './data/SSL/4695946_www.72qier.icu.key'
-SSL_PEM = '' # './data/SSL/4695946_www.72qier.icu.pem'
+SSL_KEY =  './data/SSL/4695946_www.72qier.icu.key'
+SSL_PEM = './data/SSL/4695946_www.72qier.icu.pem'
